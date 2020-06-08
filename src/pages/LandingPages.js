e8913ae30be61750915f10fbb67bd5486e1a1932
rzqrafii <rafiirizqullah23@gmail.com>
2020-06-08 10:40:55 +0700

    first commit

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import moduleName from 'elements/Button'
+
+export default class LandingPages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+rfc
